417b1fa788fe845629397fb8930b4b1c07bc13f3
Willem Rosenthal <willemrosenthal@gmail.com>
2022-09-04 16:41:17 -0400

    - added the tile layers class

---
+++ Assets/Scripts/System/TileLayers.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+public class TileLayers : MonoBehaviour {
+
+	public Color finalColor = Color.white;
+	Tilemap tileMap;
+
+
+	void Awake () {
+		tileMap = GetComponent <Tilemap> ();
+	}
+
+	void Start () {
+		tileMap.color = finalColor;
+	}
+
+}
